9ebdf6961c3456ec9b7cdaaf8a5ea5ad56072a76
vogel@e-spirit.com <vogel@e-spirit.com>
2016-10-17 10:02:13 +0200

    removed wait in example test

--- src/test/java/de/espirit/firstspirit/webedit/test/ExampleTest.java
+++ src/test/java/de/espirit/firstspirit/webedit/test/ExampleTest.java
@@ -4,7 +4,6 @@ import de.espirit.firstspirit.webedit.test.ui.AbstractUiTest;
 import de.espirit.firstspirit.webedit.test.ui.Constants;
 import de.espirit.firstspirit.webedit.test.ui.UiTestRunner;
 import de.espirit.firstspirit.webedit.test.ui.util.Assert;
-import de.espirit.firstspirit.webedit.test.ui.util.Utils;
 import de.espirit.firstspirit.webedit.test.ui.webdriver.factory.LocalPhantomJSWebDriverFactory;
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -19,8 +18,6 @@ public class ExampleTest extends AbstractUiTest {
 	public void executeTestcase() {
 		System.setProperty(Constants.PARAM_ERROR_FILE_PATH, "d:\\screenshots\\");
 		LOGGER.info("Test 'search for solar' started");
-		Utils.waitForCC();
-
 		cc().menu().search("solar");
 		Assert.assertTrue("search-results expected", cc().reports().search().getResultCount() > 0, cc().driver());
 
