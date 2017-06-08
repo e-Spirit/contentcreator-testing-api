50c4e1b9be617df1bd761b0888a2ad519aee9e8e
Philipp Börsteken <boersteken@e-spirit.com>
2017-06-08 15:18:20 +0200

    PSCCT-28 Added storing of browser console log and added logging capabilities to chrome- and phantomjs-webdriver factories

--- src/main/java/de/espirit/firstspirit/webedit/test/ui/util/ErrorHandler.java
+++ src/main/java/de/espirit/firstspirit/webedit/test/ui/util/ErrorHandler.java
@@ -1,76 +1,108 @@
 package de.espirit.firstspirit.webedit.test.ui.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
+import de.espirit.firstspirit.webedit.test.ui.Constants;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import de.espirit.firstspirit.webedit.test.ui.Constants;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class ErrorHandler {
+    private static final Logger LOGGER = Logger.getLogger(ErrorHandler.class);
 
-	private static final Logger LOGGER = Logger.getLogger(ErrorHandler.class);
+    /**
+     * Handles the error by printing logs and taking a screenshot
+     *
+     * @param webDriver
+     */
+    public static void handleError(final WebDriver webDriver) {
+        if ((Utils.env(Constants.PARAM_ERROR_FILE_PATH, Constants.DEFAULT_ERROR_FILE_PATH) != null)
+                && ((webDriver instanceof PhantomJSDriver) || (webDriver instanceof ChromeDriver))) {
+            final long timestamp = new Date().getTime();
+            ErrorHandler.takeScreenshot(timestamp, webDriver);
+            ErrorHandler.printPageSource(timestamp, webDriver);
+            ErrorHandler.printBrowserConsoleLog(timestamp, webDriver);
+        }
+    }
 
+    /**
+     * Prints the browser console log to a log file
+     *
+     * @param timestamp
+     * @param webDriver
+     */
+    private static void printBrowserConsoleLog(final long timestamp, final WebDriver webDriver) {
+        try {
+            final String fileName = ErrorHandler.getErrorFilePath() + "/browserConsoleLog-" + timestamp + ".log";
+            final File browserConsoleLogFile = new File(fileName);
 
-	private ErrorHandler() {
+            LogEntries browserConsoleLogEntries = webDriver.manage().logs().get(LogType.BROWSER);
+            String browserConsoleLog = browserConsoleLogEntries.getAll().stream()
+                    .map(LogEntry::toString)
+                    .collect(Collectors.joining("\n"));
 
-	}
+            FileUtils.write(browserConsoleLogFile, browserConsoleLog, "UTF-8");
+            ErrorHandler.LOGGER.info("Browser console log saved to " + fileName);
+        } catch (final IOException e) {
+            ErrorHandler.LOGGER.error("", e);
+        }
+    }
 
+    /**
+     * Prints the html page source to a log file
+     * @param timestamp
+     * @param webDriver
+     */
+    private static void printPageSource(final long timestamp, final WebDriver webDriver) {
+        try {
+            final String fileName = ErrorHandler.getErrorFilePath() + "/pageSource-" + timestamp + ".html";
+            final File pageSource = new File(fileName);
+            FileUtils.write(pageSource, webDriver.getPageSource(), "UTF-8");
+            ErrorHandler.LOGGER.info("Page source saved to " + fileName);
+        } catch (final IOException e) {
+            ErrorHandler.LOGGER.error("", e);
+        }
+    }
 
-	public static void handleError(final WebDriver webDriver) {
-		if ((Utils.env(Constants.PARAM_ERROR_FILE_PATH, Constants.DEFAULT_ERROR_FILE_PATH) != null) && (webDriver instanceof PhantomJSDriver)) {
-			final long timestamp = new Date().getTime();
-			ErrorHandler.takeScreenshot(timestamp, webDriver);
-			ErrorHandler.printPageSource(timestamp, webDriver);
-		}
-	}
+    /**
+     * Takes a screenshot of the current browser view
+     * This utility method only supports {@link PhantomJSDriver} and {@link ChromeDriver}
+     *
+     * @param timestamp
+     * @param webDriver
+     * @throws IllegalArgumentException thrown when the webDriver don't support screenshots
+     */
+    private static void takeScreenshot(final long timestamp, final WebDriver webDriver) throws IllegalArgumentException {
+        java.io.File screenshot = null;
+        if (webDriver instanceof PhantomJSDriver) {
+            screenshot = ((PhantomJSDriver) webDriver).getScreenshotAs(OutputType.FILE);
+        } else if (webDriver instanceof ChromeDriver) {
+            screenshot = ((ChromeDriver) webDriver).getScreenshotAs(OutputType.FILE);
+        }
+        if (screenshot == null) {
+            throw new IllegalArgumentException("webDriver don't support screenshots");
+        } else {
+            try {
+                final String fileName = ErrorHandler.getErrorFilePath() + "/screenshot-" + timestamp + ".png";
+                FileUtils.copyFile(screenshot, new File(fileName));
+                ErrorHandler.LOGGER.info("Screenshot saved to " + fileName);
+            } catch (final IOException e) {
+                ErrorHandler.LOGGER.error("", e);
+            }
+        }
+    }
 
-
-	private static void printPageSource(final long timestamp, final WebDriver webDriver) {
-		try {
-			final String fileName = ErrorHandler.getErrorFilePath() + "/pageSource-" + timestamp + ".html";
-			final File pageSource = new File(fileName);
-			FileUtils.write(pageSource, webDriver.getPageSource(), "UTF-8");
-			ErrorHandler.LOGGER.info("Page source saved to " + fileName);
-		} catch (final IOException e) {
-			ErrorHandler.LOGGER.error("", e);
-		}
-	}
-
-
-	/**
-	 * This utility only support {@link PhantomJSDriver} and {@link ChromeDriver}
-	 *
-	 * @param timestamp
-	 * @param webDriver
-	 * @throws IllegalArgumentException thrown when the webDriver don't support screenshots
-	 */
-	private static void takeScreenshot(final long timestamp, final WebDriver webDriver) throws IllegalArgumentException {
-		java.io.File screenshot = null;
-		if (webDriver instanceof PhantomJSDriver) {
-			screenshot = ((PhantomJSDriver) webDriver).getScreenshotAs(OutputType.FILE);
-		} else if (webDriver instanceof ChromeDriver) {
-			screenshot = ((ChromeDriver) webDriver).getScreenshotAs(OutputType.FILE);
-		}
-		if (screenshot == null) {
-			throw new IllegalArgumentException("webDriver don't support screenshots");
-		} else {
-			try {
-				final String fileName = ErrorHandler.getErrorFilePath() + "/screenshot-" + timestamp + ".png";
-				FileUtils.copyFile(screenshot, new File(fileName));
-				ErrorHandler.LOGGER.info("Screenshot saved to " + fileName);
-			} catch (final IOException e) {
-				ErrorHandler.LOGGER.error("", e);
-			}
-		}
-	}
-
-
-	public static String getErrorFilePath() {
-		return Utils.env(Constants.PARAM_ERROR_FILE_PATH, Constants.DEFAULT_ERROR_FILE_PATH) + '/' + Utils.env(Constants.PARAM_ERROR_SUBDIRECTORY, Constants.DEFAULT_ERROR_SUBDIRECTORY);
-	}
+    public static String getErrorFilePath() {
+        return Utils.env(Constants.PARAM_ERROR_FILE_PATH, Constants.DEFAULT_ERROR_FILE_PATH) + '/'
+                + Utils.env(Constants.PARAM_ERROR_SUBDIRECTORY, Constants.DEFAULT_ERROR_SUBDIRECTORY);
+    }
 }
--- src/main/java/de/espirit/firstspirit/webedit/test/ui/webdriver/factory/LocalChromeWebDriverFactory.java
+++ src/main/java/de/espirit/firstspirit/webedit/test/ui/webdriver/factory/LocalChromeWebDriverFactory.java
@@ -2,10 +2,15 @@ package de.espirit.firstspirit.webedit.test.ui.webdriver.factory;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
 
 import static de.espirit.firstspirit.webedit.test.ui.util.Utils.env;
 
@@ -23,7 +28,11 @@ public class LocalChromeWebDriverFactory implements WebDriverFactory {
         final String webdriverExecutable = env("webdriver.chrome.executable", DEFAULT_WEBDRIVER_EXECUTABLE);
         final boolean verbose = Boolean.valueOf(env("webdriver.chrome.verbose", DEFAULT_VERBOSITY));
         final ChromeDriverService service = new ChromeDriverService.Builder().usingDriverExecutable(new File(webdriverExecutable)).withVerbose(verbose).build();
-        return new ChromeDriver(service);
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        LoggingPreferences loggingPreferences = new LoggingPreferences();
+        loggingPreferences.enable(LogType.BROWSER, Level.ALL);
+        capabilities.setCapability(CapabilityType.LOGGING_PREFS, loggingPreferences);
+        return new ChromeDriver(service, capabilities);
     }
 
     @Override
